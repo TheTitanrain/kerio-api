ada8783bf579f8ede4b5db1322a439323a64ad05
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-04-25 17:03:18 +0400

    Add structs

--- connect/client/client.go
+++ connect/client/client.go
@@ -1,5 +1,11 @@
 package client
 
+type (
+	Watermark    int
+	Kld          int
+	PriorityType string
+)
+
 // LocalizableMessage Message can contain replacement marks: { "User %1 cannot be deleted.", ["jsmith"], 1 }
 type LocalizableMessage struct {
 	Message              string   `json:"message"`
@@ -26,6 +32,50 @@ type AddResult struct {
 	ErrorMessage LocalizableMessage `json:"errorMessage"`
 }
 
+// CreateResult - Details about a particular item created.
+type CreateResult struct {
+	InputIndex int       `json:"inputIndex"`
+	Id         int       `json:"id"`
+	Watermark  Watermark `json:"watermark"`
+}
+
+// SetResult - Details about a particular item created.
+type SetResult struct {
+	InputIndex int       `json:"inputIndex"`
+	Id         Kld       `json:"id"`
+	Watermark  Watermark `json:"watermark"`
+}
+
+// Image - [READ ONLY] Id of uploaded image.
+type Image struct {
+	Url string `json:"url"`
+	Id  string `json:"id"`
+}
+
+// Attachment
+type Attachment struct {
+	Id          string `json:"id"`
+	Url         string `json:"url"`
+	Name        string `json:"name"`
+	ContentType string `json:"contentType"`
+	ContentId   string `json:"contentId"`
+	Size        int    `json:"size"`
+}
+
+type Attendee struct {
+	DisplayName string `json:"displayName"`
+	Role        string `json:"role"`
+	IsNotified  bool   `json:"isNotified"`
+	PartStatus  string `json:"partStatus"`
+}
+
+type OperatorExtension struct {
+	ExtensionId  string `json:"extensionId"`
+	TelNum       string `json:"telNum"`
+	Description  string `json:"description"`
+	IsRegistered bool   `json:"isRegistered"`
+}
+
 // ApiApplication - Describes client (third-party) application or script which uses the Administration API
 type ApiApplication struct {
 	Name    string `json:"name"`
@@ -40,6 +90,54 @@ type Error struct {
 	MessageParameters LocalizableMessageParameters `json:"messageParameters"`
 }
 
+// NamedValue pair Note: all fields must be assigned if used in set methods
+type NamedValue struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type DisplayablePart struct {
+	ContentType string `json:"contentType"`
+	Content     string `json:"content"`
+}
+
+type Email struct {
+	Address   string `json:"address"`
+	Name      string `json:"name"`
+	ContactId string `json:"contactId"`
+}
+
+type Mail struct {
+	From             Email             `json:"from"`
+	Sender           Email             `json:"sender"`
+	DisplayableParts []DisplayablePart `json:"displayableParts"`
+	To               []Email           `json:"to"`
+	Cc               []Email           `json:"cc"`
+	Bcc              []Email           `json:"bcc"`
+	ReplyTo          []Email           `json:"replyTo"`
+	NotificationTo   Email             `json:"notificationTo"`
+	Subject          string            `json:"subject"`
+	Priority         PriorityType      `json:"priority"`
+	Size             int               `json:"size"`
+	Encrypt          bool              `json:"encrypt"`
+	IsAnswered       bool              `json:"isAnswered"`
+	IsDraft          bool              `json:"isDraft"`
+	IsFlagged        bool              `json:"isFlagged"`
+	IsForwarded      bool              `json:"isForwarded"`
+	IsJunk           bool              `json:"isJunk"`
+	IsMDNSent        bool              `json:"isMdnSent"`
+	IsReadOnly       bool              `json:"isReadOnly"`
+	IsSeen           bool              `json:"isSeen"`
+	RequestDSN       bool              `json:"requestDsn"`
+	Send             bool              `json:"send"`
+	ShowExternal     bool              `json:"showExternal"`
+	Sign             bool              `json:"sign"`
+}
+
+type OutIsEligible struct {
+	IsEligible bool `json:"isEligible"`
+}
+
 // ABExtension Extension of Apple Address Book
 type ABExtension struct {
 	GroupID string `json:"groupId"`
@@ -47,8 +145,8 @@ type ABExtension struct {
 }
 
 type AccountSyncKey struct {
-	Guid      string `json:"guid"`
-	Watermark string `json:"watermark"`
+	Guid      string    `json:"guid"`
+	Watermark Watermark `json:"watermark"`
 }
 
 // UserInfo - Details of the logged user into the webmail
